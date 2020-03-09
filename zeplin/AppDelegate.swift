8f434866543d7d4fe0f524dee9f25d47e183fcdf
Gokhan Namal <gokhanamal@gmail.com>
2020-03-09 19:10:03 +0300

    Fix google auth user agent error by setting fake user agent

--- zeplin/AppDelegate.swift
+++ zeplin/AppDelegate.swift
@@ -30,9 +30,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 self.window?.makeKeyAndVisible()
             })
             .disposed(by: bag)
-
+        
         window.rootViewController = rootController
-      window.backgroundColor = Colors.windowBackgroundBlack.color
+        window.backgroundColor = Colors.windowBackgroundBlack.color
         window.makeKeyAndVisible()
         return window
     }()
@@ -43,6 +43,8 @@ extension AppDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = splashWindow
+        
+        UserDefaults.standard.register(defaults: ["UserAgent" :"Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"])
         
         return true
     }
